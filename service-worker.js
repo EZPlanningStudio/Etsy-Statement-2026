a98a7ff9cd17bb137752a619b6bce18b2b22e712
EZPlanningStudio <daniellanechita@gmail.com>
2026-07-01 12:29:56 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'etsy-statement-2026-v2';
+const CACHE_NAME = 'etsy-statement-2026-v4';
 const FILES_TO_CACHE = [
   '.',
   'index.html',
@@ -23,6 +23,12 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
